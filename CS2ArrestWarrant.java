a8cc85d0693d5fc6333d200b2338a72dc1896d30
stevelouis4848 <33739350+stevelouis4848@users.noreply.github.com>
2018-11-08 19:50:27 -0500

    Set uo the frustrating adjacency list nothing tested yet

--- CS2ArrestWarrant.java
+++ CS2ArrestWarrant.java
@@ -27,12 +27,12 @@ public class CS2ArrestWarrant {
         int destination, edge_Cost;
         
         private Adj_City(int a, int b){
-            a = destination;
-            b = edge_Cost;
+            destination = a;
+            edge_Cost = b;
         }
-   	}
+    }
 
-	public static class City_State{
+    public static class City_State{
         
         int id, passengers, current_Cost;
         
@@ -41,13 +41,14 @@ public class CS2ArrestWarrant {
             passengers = b;
             current_Cost = c;
         }
-   	}
+    }
 
     public static void main(String[] args) {
           
         // reading in the number of ciries and roads.
         num_Of_Cities = input.nextInt();
         num_Of_Roads = input.nextInt();
+        
         
         //Adjaency List of int arrayList ints of the id of the ciies connected
         ArrayList<ArrayList<Adj_City>> adjacencyList = new ArrayList<>(num_Of_Cities);
@@ -58,8 +59,30 @@ public class CS2ArrestWarrant {
 
         // Read the roads and store them in the adjacency list right away.
         nodes(num_Of_Pirates_Array, bribe_Cost_Array);
+        
+        /*
+        for(int i = 0; i < num_Of_Cities; i++){
+            System.out.print(" Pirates[i]: " + num_Of_Pirates_Array[i]);
+            System.out.print(" bribe Costi]: " + bribe_Cost_Array[i] + "\n");
+        }
+        */
+        
         edges(adjacencyList);
+        System.out.print(" adjList Size: " + adjacencyList.size() + "\n");
+        
+        /*
+        for(int i = 0; i < num_Of_Cities; i++){
+
+            for(int j = 0; j < adjacencyList.get(i).size(); j++){
+
+                System.out.print("Destination[" + i + 1 + "]: " + adjacencyList.get(i).get(j).destination + " Cost[" + i + 1 + "]: " + adjacencyList.get(i).get(j).edge_Cost + "\n");
+            }
+          
+        }
+        */
+        
         dijkstra(adjacencyList, num_Of_Pirates_Array, bribe_Cost_Array);
+        
     }
     
     public static void  nodes(int[]  num_Of_Pirates_Array, int[] bribe_Cost_Array){
@@ -74,14 +97,23 @@ public class CS2ArrestWarrant {
 
         int city1, city2, cost;
 
+        // Intitlializes each index od the arrayList tot a new ArrayList
+        for(int i = 0; i < num_Of_Cities; i++){
+            adjacencyList.add(new ArrayList<Adj_City>());
+        }
+        
+        // Sets each of the slots with the proper adjacent city
         for(int i = 0; i < num_Of_Roads; i++){
+            
             city1 = input.nextInt();
             city2 = input.nextInt();
             cost = input.nextInt();
             
-            adjacencyList.get(city1).add(new Adj_City(city2, cost));
-            adjacencyList.get(city2).add(new Adj_City(city1, cost));       
+            adjacencyList.get(city1 - 1).add(new Adj_City(city2, cost));
+            
+            adjacencyList.get(city2 - 1).add(new Adj_City(city1, cost));       
         }
+        
     }
 
     public static void dijkstra( ArrayList<ArrayList<Adj_City>> adjacencyList, int[]  num_Of_Pirates_Array, int[] bribe_Cost_Array){
@@ -107,38 +139,38 @@ public class CS2ArrestWarrant {
         
         // Repeat until the destination city has been added to the visited list;
         while(current_City_Id!= final_Destination){
-	        /*  Form your current node check all the other adjacent nodes,that have not been visited
-	            with those, put those states into the PQ with updated costs.      
-	        */
+            /*  Form your current node check all the other adjacent nodes,that have not been visited
+                with those, put those states into the PQ with updated costs.      
+            */
 
-	        // The length of all adjacent cities (very inefficient Order (n).
-	        num_Of_AdjCities = adjacencyList.get(current_City_Id).size();
-	        
-	        //	For all those adjacent cities go through the list and consider them.(Order (n))
-	        for (int i = 0; i < num_Of_AdjCities; i++){
-	            
+            // The length of all adjacent cities (very inefficient Order (n).
+            num_Of_AdjCities = adjacencyList.get(current_City_Id).size();
+            
+            //  For all those adjacent cities go through the list and consider them.(Order (n))
+            for (int i = 0; i < num_Of_AdjCities; i++){
+                
 
-	           adj_City_Id = adjacencyList.get(current_City_Id).get(i).destination;
+               adj_City_Id = adjacencyList.get(current_City_Id).get(i).destination;
                    adj_City_Cost = adjacencyList.get(current_City_Id).get(i).edge_Cost;
 
-	            
-	           // If we have not visited city state yet.
-	           // Update the cost of the city and add it to the priority queue.
-	           if(visited[adj_City_Id][num_Of_Passengers] != 0){
+                
+               // If we have not visited city state yet.
+               // Update the cost of the city and add it to the priority queue.
+               if(visited[adj_City_Id][num_Of_Passengers] != 0){
 
                         // NO ned to compare might end up with duplicate states in the PQ
                         considered_City = new City_State(adj_City_Id, num_Of_Passengers, overall_Cost + (num_Of_Passengers * adj_City_Cost) );
                         dijkstraQueue.add(considered_City);  
-	           }	            
-	        }
-	        
-	        //  Pop the top of the priority queue,and add it to the visited List.
-	        pop_City = dijkstraQueue.poll();
-	        visited[pop_City.id][pop_City.passengers] = 1;
+               }                
+            }
+            
+            //  Pop the top of the priority queue,and add it to the visited List.
+            pop_City = dijkstraQueue.poll();
+            visited[pop_City.id][pop_City.passengers] = 1;
 
-	        //Update the current city id and the overall cost so far.
-	        current_City_Id = pop_City.id;
-	        overall_Cost = pop_City.current_Cost;
+            //Update the current city id and the overall cost so far.
+            current_City_Id = pop_City.id;
+            overall_Cost = pop_City.current_Cost;
         }
     }
 }
